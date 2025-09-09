85a316a1c8ed926deb4d57f13ee9212e4966d1d8
Seooon <jss67870@gmail.com>
2025-09-09 10:44:22 +0900

    0909 수업실습

---
+++ Ch02/src/sec02/exam1/IntegerLitaralExample.java
@@ -0,0 +1,18 @@
+package sec02.exam1;
+
+public class IntegerLitaralExample {
+
+	public static void main(String[] args) {
+		int var1 = 0b1011;
+		int var2 = 0206;
+		int var3 = 365;
+		int var4 = 0x83;
+		
+		System.out.println("var1 = " + var1);
+		System.out.println("var2 = " + var2);
+		System.out.println("var3 = " + var3);
+		System.out.println("var4 = " + var4);
+
+	}
+
+}
