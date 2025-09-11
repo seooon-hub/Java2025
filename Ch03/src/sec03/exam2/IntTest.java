ca6ae3452c6bbfde70be7ec360e408b0e19b7aca
Seooon <jss67870@gmail.com>
2025-09-11 14:48:05 +0900

    0911 Java 강의
    0911 Java 강의

---
+++ Ch03/src/sec03/exam2/IntTest.java
@@ -0,0 +1,31 @@
+package sec03.exam2;
+
+import java.util.Scanner;
+
+public class IntTest {
+
+	public static void main(String[] args) {
+		byte val1 = 1;
+		byte val2 = 2;
+		
+		//byte val3= val1 + val2;
+		
+//		System.out.println("1/2" + 1/2);
+//		System.out.println("1,0/2.0" + 1.0/2.0);
+		
+		String str = "10";
+		int value = 20;
+		String str1 = str + value; 
+//		int result = val1 + Integer.parseInt(str);
+//		System.out.println(result);
+		
+		System.out.println(str1);
+			
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		System.out.println(input);
+		
+		
+	}
+
+}
