a1d143e2e8aa5b0955c8decc59d267117559093b
Seooon <jss67870@gmail.com>
2025-09-16 10:39:05 +0900

    0916 수업 실습
    0916 수업 실습

---
+++ Ch03/src/sec03/exam3/ValueTest.java
@@ -0,0 +1,21 @@
+package sec03.exam3;
+
+public class ValueTest {
+
+	public static void main(String[] args) {
+		int value = 10;
+		String str = "3" + 7 + 8;
+		//			 "37" + 8
+		
+		str = 7 + 8 + "3";
+		// 		15 +"3"
+		
+		str = "" + value;
+		// value를 int -> String
+		
+		System.out.println(str);
+		
+
+	}
+
+}
