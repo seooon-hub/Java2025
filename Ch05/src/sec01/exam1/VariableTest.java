0cb7c2dda50666e782b9a0fa7842cbe33c387682
Seooon <jss67870@gmail.com>
2025-09-23 10:47:53 +0900

    0923 강의 실습
    0923 강의 실습

---
+++ Ch05/src/sec01/exam1/VariableTest.java
@@ -0,0 +1,15 @@
+package sec01.exam1;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int a = null; // int에서 null은 처리x
+		int b = a;
+		
+		String str1 = null;
+		String str2 = str1;
+
+		System.out.println("Hi")
+	}
+
+}
